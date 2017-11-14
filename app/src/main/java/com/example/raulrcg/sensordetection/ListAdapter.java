8918946ef44c6872286026b686aef9c761788618
Raúl Rafael Carvajal Gutierrez <raracarvajal@ittepic.edu.mx>
2017-11-13 20:40:14 -0700

    Initial commit

---
+++ app/src/main/java/com/example/raulrcg/sensordetection/ListAdapter.java
@@ -0,0 +1,65 @@
+package com.example.raulrcg.sensordetection;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by raulrcg on 13/11/17.
+ */
+
+public class ListAdapter extends BaseAdapter {
+
+    Context context;
+    String arr[];
+    LayoutInflater inflater;
+
+    public ListAdapter(Context context,String arr[]){
+        this.context=context;
+        this.arr=arr;
+    }
+
+    @Override
+    public int getCount() {
+        return arr.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return arr[i];
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        // Declare Variables
+        TextView nom;
+        TextView mar;
+        TextView ver;
+
+        //http://developer.android.com/intl/es/reference/android/view/LayoutInflater.html
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View itemView = inflater.inflate(R.layout.list_adapter, viewGroup, false);
+
+        // Locate the TextViews in listview_item.xml
+        nom = (TextView) itemView.findViewById(R.id.nombre);
+        mar = (TextView) itemView.findViewById(R.id.marca);
+        ver = (TextView) itemView.findViewById(R.id.version);
+
+        // Capture position and set to the TextViews
+        String irt[]=arr[i].split("/");
+        nom.append(irt[0]);
+        mar.append(irt[1]);
+        ver.append(irt[2]);
+
+        return itemView;
+    }
+}
---
+++ app/src/main/java/com/example/raulrcg/sensordetection/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.raulrcg.sensordetection;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SensorManager sensorManager;
+    //TextView textView=null;
+
+    ListView lv;
+    ListAdapter adp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //textView=(TextView)findViewById(R.id.txt);
+        lv=(ListView)findViewById(R.id.lista);
+
+        sensorManager = (SensorManager)this.getSystemService(SENSOR_SERVICE);
+        List<Sensor> lista=sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        StringBuilder data = new StringBuilder();
+        for (int i=1;i<lista.size();i++){
+            data.append(lista.get(i).getName()+"/");
+            data.append(lista.get(i).getVendor()+"/");
+            data.append(lista.get(i).getVersion()+"/\n");
+        }
+
+        String arr[]=data.toString().split("\n");
+        //textView.setText(data);
+
+        adp = new ListAdapter(getApplicationContext(),arr);
+        lv.setAdapter(adp);
+
+    }
+}
